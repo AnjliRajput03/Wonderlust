5a5b434a74fa9688834ea7ac7018f1c9d4f4c015
Anjli Rajput <anjlirajput324@gmail.com>
2024-11-23 16:41:45 +0530

    first commit

--- app.js
+++ app.js
@@ -17,31 +17,36 @@ const LocalStrategy = require("passport-local");
 
 const User = require("./models/user.js");
 
+
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
+
 main()
   .then(() => {
-    console.log("connected to DB");
+    console.log("Connected to DB");
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Database Connection Error:", err);
   });
 
 async function main() {
   await mongoose.connect(dbUrl);
 }
 
-app.set("views engine", "ejs");
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
@@ -50,8 +55,9 @@ const store = MongoStore.create({
   },
   touchAfter: 24 * 3600,
 });
-store.on("error", () => {
-  console.log("ERROR in MONGO SESSION STORE", err);
+
+store.on("error", (err) => {
+  console.log("Error in Mongo Session Store:", err);
 });
 
 const sessionOptions = {
@@ -66,12 +72,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send(" Hi  I am Wonderlust");
-// });
-
 app.use(session(sessionOptions));
 app.use(flash());
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -80,41 +83,32 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get('/listings', (req, res) => {
-  const currUser = req.user; // Assuming `req.user` contains user data
-  res.render('listings', { currUser }); // Pass `currUser` to the EJS template
-});
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
-
   next();
 });
 
-// app.get("/demouser",async(req,res)=>{
-//     let FakeUser = new User({
-//         email : "student@gmail.com",
-//         username : "delta-student"
-//     });
-
-//     let registeredUser = await User.register(FakeUser,"hello world");
-//     res.send(registeredUser);
-// });
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page not found !"));
+
+app.get("/", (req, res, next) => {
+  res.redirect("/listings"); // Redirect root to `/listings`
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  let { statusCode = 500, message = "Something went wrong !" } = err;
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not found!"));
+});
+
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong!" } = err;
   res.status(statusCode).render("error.ejs", { message });
 });
 
